d6cd5236e56464e9b3d951c01c0cafe7eb8d2049
Riley Slayden <slaydenriley@gmail.com>
2020-01-23 16:45:05 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :characters do |t|
       t.string :first_name
-      t.string :last_name
+  
     end
   end
 end
